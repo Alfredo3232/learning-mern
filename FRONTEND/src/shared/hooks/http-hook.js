0e8525c219619ca2658b78c0f883b72c664205dd
arojas32 <Alfredo2001@me.com>
2022-06-29 20:51:12 -0700

    changes

--- FRONTEND/src/shared/hooks/http-hook.js
+++ FRONTEND/src/shared/hooks/http-hook.js
@@ -1,16 +1,23 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef, useEffect } from "react";
 
 export const useHttpClient = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState();
 
+    const activeHttpRequests = useRef([]);
+
     const sendRequest = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
         setIsLoading(true);
+
+        const httpAbortCtrl = new AbortController();
+        activeHttpRequests.current.push(httpAbortCtrl);
+
         try {
             const response = await fetch(url, {
                 method,
                 body,
-                headers
+                headers,
+                signal: httpAbortCtrl.signal
             });
 
             const responseData = await response.json();
@@ -28,6 +35,8 @@ export const useHttpClient = () => {
     const clearError = () => {
         setError(null);
     }
+
+    useEffect(() => {}, []);
 
     return { isLoading, error, sendRequest, clearError };
 }

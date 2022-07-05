2a0a2d28a4b11b685a46eaee1c02b761c076a3e0
arojas32 <Alfredo2001@me.com>
2022-07-04 20:22:47 -0700

    made progress and fixed some errors

--- BACKEND/controllers/users-controllers.js
+++ BACKEND/controllers/users-controllers.js
@@ -106,7 +106,7 @@ const login = async (req, res, next) => {
   if (!existingUser) {
     const error = new HttpError(
       'Invalid credentials, could not log you in.',
-      401
+      403
     );
     return next(error);
   }
@@ -125,7 +125,7 @@ const login = async (req, res, next) => {
   if (!isValidPassword) {
     const error = new HttpError(
       'Invalid credentials, could not log you in.',
-      401
+      403
     );
     return next(error);
   }

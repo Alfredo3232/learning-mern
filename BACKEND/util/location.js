7e6442275920bea4a15c2a3f74c27419e322fbe6
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-07-04 20:56:46 -0700

    addded security and progress

--- BACKEND/util/location.js
+++ BACKEND/util/location.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = 'AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA';
+const API_KEY = process.env.GOOGLE_API_KEY;
 
 async function getCoordsForAddress(address) {
   const response = await axios.get(
